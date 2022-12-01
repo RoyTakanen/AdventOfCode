b2533516352cb2fac401d703f9f7f4404b99e0f7
RoyTakanen <roy@imap.fi>
2022-12-01 21:54:13 +0200

    add day 1

---
+++ 2022/1/part2.py
@@ -0,0 +1,13 @@
+lines = open("input").read().splitlines()
+
+carrying = []
+
+elf = 0
+for idx, line in enumerate(lines):
+    if not line:
+        carrying.append(elf)
+        elf = 0
+        continue
+    elf += int(line)
+
+print(sum(sorted(carrying, reverse=True)[0:3]))
